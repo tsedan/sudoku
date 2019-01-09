1f1ebd423f32de2eafd81367154b184e897fa57a
Tomer Sedan <tomer.sedan@gmail.com>
2019-01-08 18:26:05 -0800

    Button Fixes

--- src/Modal.js
+++ src/Modal.js
@@ -68,7 +68,7 @@ const Modal = props => {
               <td className="optionData" onClick={() => { props.handleAddNum(7) }}><span className="buttonEvents">7</span></td>
               <td className="optionData" onClick={() => { props.handleAddNum(8) }}><span className="buttonEvents">8</span></td>
               <td className="optionData" onClick={() => { props.handleAddNum(9) }}><span className="buttonEvents">9</span></td>
-              <td className="optionData" onClick={() => { props.handleAddNum(0) }}><span className="buttonEvents">&#10006;</span></td>
+              <td className="optionData" onClick={() => { props.handleAddNum(0) }}><span className="buttonEvents">&#x2716;</span></td>
             </tr>
           </tbody>
         </table>
