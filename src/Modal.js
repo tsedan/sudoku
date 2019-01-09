6b8b19fa9a57b7c624ab3fa277e5751e1af90a06
Tomer Sedan <tomer.sedan@gmail.com>
2019-01-08 19:16:18 -0800

    Fixed Close Svg

--- src/Modal.js
+++ src/Modal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import KeyHandler, { KEYPRESS } from 'react-key-handler';
 import './App.css';
 
-const Modal = props => {
+const Modal = props => { //352 512
   return (
     <div className="modal">
       <KeyHandler
@@ -68,7 +68,9 @@ const Modal = props => {
               <td className="optionData" onClick={() => { props.handleAddNum(7) }}><span className="buttonEvents">7</span></td>
               <td className="optionData" onClick={() => { props.handleAddNum(8) }}><span className="buttonEvents">8</span></td>
               <td className="optionData" onClick={() => { props.handleAddNum(9) }}><span className="buttonEvents">9</span></td>
-              <td className="optionData" onClick={() => { props.handleAddNum(0) }}><span className="buttonEvents">&#x2716;</span></td>
+              <td className="optionData" onClick={() => { props.handleAddNum(0) }}><span className="buttonEvents">
+                <svg aria-hidden="true" data-prefix="fas" data-icon="times" className="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="-130 -60 600 600"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
+              </span></td>
             </tr>
           </tbody>
         </table>
